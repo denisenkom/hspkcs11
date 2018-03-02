1ff4c56cffebffb5e47dd63f66863cadd9c2e164
denisenk <denisenk@amazon.com>
2018-03-02 18:57:34 -0500

    Travis: installing softhsm package

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -7,3 +7,15 @@ export PATH=$HOME/.local/bin:/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
 # Download and unpack the stack executable
 mkdir -p ~/.local/bin
 travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+
+# Download and build softhsm package
+mkdir -p ~/.local/softhsm
+cd ~/.local/softhsm
+wget https://dist.opendnssec.org/source/softhsm-2.3.0.tar.gz
+tar -xf softhsm-2.3.0.tar.gz
+rm softhsm-2.3.0.tar.gz
+cd softhsm-2.3.0
+./configure --prefix=$HOME/.local/
+make
+cd ..
+rm -r softhsm-2.3.0
