abbbbe774c1c4d42dece8878cc6fb45d8d9357e6
denisenk <denisenk@amazon.com>
2018-03-02 18:32:13 -0500

    Moved Travis before install steps into a script.

---
+++ travis/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt-get -qq update
+
+unset CC
+export PATH=$HOME/.local/bin:/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
+# Download and unpack the stack executable
+mkdir -p ~/.local/bin
+travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
