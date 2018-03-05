1f252aa8f52ed853601d9969700d2237096ffe80
denisenk <denisenk@amazon.com>
2018-03-04 20:29:57 -0500

    fixing build script

--- appveyor/before_test.cmd
+++ appveyor/before_test.cmd
@@ -2,9 +2,12 @@ ECHO ON
 
 SET PATH=C:\Program Files\Git\mingw64\bin;%PATH%
 
-IF NOT EXIST C:\Users\appveyor\AppData\Local\Programs\stack (
+IF NOT EXIST stack.exe (
     curl -sS -ostack.zip -L --insecure http://www.stackage.org/stack/windows-i386
     7z x stack.zip stack.exe
+)
+
+IF NOT EXIST C:\Users\appveyor\AppData\Local\Programs\stack (
     stack setup > nul
 )
 
