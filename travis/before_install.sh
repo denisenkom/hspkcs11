0df335efc7ea76d85553f27643a7067730db892f
denisenk <denisenk@amazon.com>
2018-03-10 00:09:42 -0500

    Fixing travis build

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+source travis_retry.sh
+
 # Download and unpack the stack executable
 mkdir -p ~/.local/bin
 travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
