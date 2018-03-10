564d2f2362bae2ac598f8c2a8452a4beda66b0bb
denisenk <denisenk@amazon.com>
2018-03-10 00:04:28 -0500

    Fixing travis build

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-unset CC
-export PATH=$HOME/.local/bin:/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
 # Download and unpack the stack executable
 mkdir -p ~/.local/bin
 travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
